f44024029e473ed5035f90bbf7010a2117c0667e
Ninh <95522691+ninhnguyen01@users.noreply.github.com>
2026-02-01 00:49:53 -0800

    Add files via upload

---
+++ TS-compilePractice/src/greetBot.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// variable declaration
+let bot = "teacherBot";
+let botLocation = "the universe";
+let botIntroduction = "My name is " + bot + ".";
+let botLocationSentence = "I live in " + botLocation + ".";
+let botAltName = "professorBot";
+let nicknameIntroduction = "My nickname is " + botAltName + ".";
+let botchosenName = "awesomeTeacherBot";
+let newNicknameGreeting = "I love my nickname but I wish people would call me " + botchosenName + ".";
+let favoriteSubject = "Computer Science";
+let favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+// print to console
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+console.log("Allow me to introduce myself.");
+console.log(botIntroduction);
+console.log(botLocationSentence);
+console.log(nicknameIntroduction);
+console.log(newNicknameGreeting);
+console.log(favoriteSubjectSentence);
+console.log("Well, it was nice to talk to you. Have a nice day!");
+//# sourceMappingURL=greetBot.js.map
---
+++ TS-compilePractice/src/greetBot.ts
@@ -0,0 +1,23 @@
+// variable declaration
+let bot: String = "teacherBot";
+let botLocation: String = "the universe"
+let botIntroduction: String = "My name is " + bot + ".";
+let botLocationSentence: String = "I live in " + botLocation + ".";
+let botAltName: String = "professorBot";
+let nicknameIntroduction: String = "My nickname is " + botAltName + ".";
+let botchosenName: String = "awesomeTeacherBot";
+let newNicknameGreeting: String = "I love my nickname but I wish people would call me " + botchosenName + ".";
+let favoriteSubject: String = "Computer Science";
+let favoriteSubjectSentence: String = "My favorite subject is " + favoriteSubject + ".";
+
+ // print to console
+console.log("Hi there!")
+console.log("I am excited to talk to you.")
+console.log("Allow me to introduce myself.")
+
+console.log(botIntroduction)
+console.log(botLocationSentence)
+console.log(nicknameIntroduction)
+console.log(newNicknameGreeting)
+console.log(favoriteSubjectSentence)
+console.log("Well, it was nice to talk to you. Have a nice day!");
